989ebf9c569049c1eb30a10dde88c5f0795ba198
Vitor Dino <vitor@kunstdesign.com.br>
2018-05-25 02:37:19 -0300

    /img to /assets/images

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
 		{
 			resolve: 'gatsby-source-filesystem',
 			options: {
-				path: `${__dirname}/src/img`,
+				path: `${__dirname}/src/assets/images`,
 				name: 'images',
 			},
 		},
