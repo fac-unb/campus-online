67da4726ac1a2bded96cb9483a2145cc768d3546
Leonardo Dino <leonardo@kunstdesign.com.br>
2018-05-27 07:10:20 -0300

    better todo comment

--- src/templates/author/index.js
+++ src/templates/author/index.js
@@ -6,6 +6,7 @@ import Content from '../../components/Content'
 // i've ported the earlier structure, it's probably wrong though,
 // most styling and structure should be inside this file,
 // and general page layout must be inside a layout, in layouts folder
+// https://www.gatsbyjs.org/docs/creating-and-modifying-pages/#choosing-the-page-layout
 
 const Author = ({name, semester, content}) => (
 	<section>
--- src/templates/editorial/index.js
+++ src/templates/editorial/index.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types'
 // i've ported the earlier structure, it's probably wrong though,
 // most styling and structure should be inside this file,
 // and general page layout must be inside a layout, in layouts folder
+// https://www.gatsbyjs.org/docs/creating-and-modifying-pages/#choosing-the-page-layout
 
 export const Editorial = ({name, semester, color}) => (
 	<section style={{color: color}}>
