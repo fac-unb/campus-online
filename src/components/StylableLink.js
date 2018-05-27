5f07e52c8b9a6813c81baadeb1bdaa28fa53fb57
Leonardo Dino <leonardo@kunstdesign.com.br>
2018-05-27 07:21:13 -0300

    eslint: enable react/no-children-prop

--- src/components/StylableLink.js
+++ src/components/StylableLink.js
@@ -3,7 +3,9 @@ import Link from 'gatsby-link'
 
 const StylableLink = ({to, children, className, style}) => (
 	// this component filters unwanted props
-	<Link to={to} className={className} style={style} children={children} />
+	<Link to={to} className={className} style={style}>
+		{children}
+	</Link>
 )
 
 export default StylableLink
