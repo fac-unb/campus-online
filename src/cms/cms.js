9a5d262bb3b5daf9ef7770a81fd4e1431633617e
Vitor Dino <vitor@kunstdesign.com.br>
2018-05-30 06:17:54 -0300

    remove boiler junk

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,11 +1,9 @@
 import CMS from 'netlify-cms'
 import AboutPagePreview from '../templates/about-page/preview'
 import BlogPostPreview from '../templates/blog-post/preview'
-import ProductPagePreview from '../templates/product-page/preview'
 import withStyleSheet from './withStyleSheet'
 
 window.___loader = window.___loader || {enqueue: () => undefined}
 
 CMS.registerPreviewTemplate('about', withStyleSheet(AboutPagePreview))
-CMS.registerPreviewTemplate('products', withStyleSheet(ProductPagePreview))
 CMS.registerPreviewTemplate('blog', withStyleSheet(BlogPostPreview))
