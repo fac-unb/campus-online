8a40a1b96ea80111d025e981bd4692458f29d960
Leonardo Dino <leonardo@kunstdesign.com.br>
2018-06-08 03:40:17 -0300

    StylableLink: handle external urls

--- src/components/StylableLink.js
+++ src/components/StylableLink.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const StylableLink = ({to, children, className, style}) => (
-	// this component filters unwanted props
-	<Link to={to} className={className} style={style}>
-		{children}
-	</Link>
-)
+const StylableLink = ({to, children, className, style}) => {
+	const href = (to || {}).pathname || (typeof to === 'string' ? to : '/')
+	const external = /^[^./]/.test(href)
+	const allowedProps = {className, style, children}
+	return external ? (
+		<a {...allowedProps} href={href} />
+	) : (
+		<Link {...allowedProps} to={to} />
+	)
+}
 
 export default StylableLink
