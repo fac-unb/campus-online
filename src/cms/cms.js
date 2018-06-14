4b59049b05cd8e1f0c682994b1a934b96df73253
Leonardo Dino <leonardo@kunstdesign.com.br>
2018-06-14 10:48:53 -0300

    cms: disable routing inside preview

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,16 +1,24 @@
 import CMS from 'netlify-cms'
+import {nest, compose} from 'recompose'
+import {StaticRouter} from 'react-router'
 import AboutPagePreview from '../templates/about-page/preview'
 import BlogPostPreview from '../templates/blog-post/preview'
 import AuthorWidget from './widgets/Author'
 import EditorialWidget from './widgets/Editorial'
 import SemesterWidget from './widgets/Semester'
-
 import withStyleSheet from './withStyleSheet'
+
+const withStaticRouting = Component => nest(StaticRouter, Component)
 
 window.___loader = window.___loader || {enqueue: () => undefined}
 
-CMS.registerPreviewTemplate('about', withStyleSheet(AboutPagePreview))
-CMS.registerPreviewTemplate('blog', withStyleSheet(BlogPostPreview))
+const enhance = compose(
+	withStyleSheet,
+	withStaticRouting,
+)
+
+CMS.registerPreviewTemplate('about', enhance(AboutPagePreview))
+CMS.registerPreviewTemplate('blog', enhance(BlogPostPreview))
 CMS.registerWidget('author', AuthorWidget)
 CMS.registerWidget('editorial', EditorialWidget)
 CMS.registerWidget('semester', SemesterWidget)
