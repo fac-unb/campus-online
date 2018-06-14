5fdf7d3795fce866cdbe8c536435d7ac07d1cf68
Leonardo Dino <leonardo@kunstdesign.com.br>
2018-06-14 09:24:15 -0300

    widgets/Semester: implement from scratch

--- src/cms/widgets/Semester/index.js
+++ src/cms/widgets/Semester/index.js
@@ -1,31 +1,53 @@
-import React, {forwardRef} from 'react'
-import CMS from 'netlify-cms'
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 
-const Relation = CMS.getWidget('relation')
-
-function isValid() {
-	const {value, field} = this.props
-	const required = field.get('required')
-	const error = {
-		type: 'CUSTOM',
-		error: true,
-		message: field.get('error') || 'precisa ser um semestre válido',
+export default class SemesterControl extends Component {
+	static defaultProps = {value: ''}
+	static propTypes = {
+		onChange: PropTypes.func.isRequired,
+		forID: PropTypes.string,
+		value: PropTypes.node,
+		classNameWrapper: PropTypes.string.isRequired,
+		setActiveStyle: PropTypes.func.isRequired,
+		setInactiveStyle: PropTypes.func.isRequired,
 	}
 
-	if (typeof value !== 'string') return error
-	if (!required && !value) return true
-	if (value.length !== 6) return error
-	if (!/^\d{4}\/\d$/.test(value)) return error
-	return true
+	isValid() {
+		const {value, field} = this.props
+		const required = field.get('required')
+		const error = {
+			type: 'CUSTOM',
+			error: true,
+			message: field.get('error') || 'precisa ser um semestre válido',
+		}
+
+		if (typeof value !== 'string') return error
+		if (!required && !value) return true
+		if (value.length !== 6) return error
+		if (!/^\d{4}\/\d$/.test(value)) return error
+		return true
+	}
+
+	render() {
+		const {
+			forID,
+			value,
+			onChange,
+			classNameWrapper,
+			setActiveStyle,
+			setInactiveStyle,
+		} = this.props
+
+		return (
+			<input
+				type="text"
+				id={forID}
+				className={classNameWrapper}
+				value={value || ''}
+				onChange={e => onChange(e.target.value)}
+				onFocus={setActiveStyle}
+				onBlur={setInactiveStyle}
+			/>
+		)
+	}
 }
-
-const methods = {
-	isValid,
-	shouldRenderSuggestions: () => false,
-}
-
-const SemesterControl = forwardRef((props, ref) => (
-	<Relation.control ref={ref} {...props} {...methods} />
-))
-
-export default SemesterControl
