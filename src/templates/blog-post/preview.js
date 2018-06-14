ab4d71737df81b015e6225ac9d5f445689aef285
Leonardo Dino <leonardo@kunstdesign.com.br>
2018-06-14 04:20:59 -0300

    templates/blog-post: fix preview

--- src/templates/blog-post/preview.js
+++ src/templates/blog-post/preview.js
@@ -1,16 +1,20 @@
-import React from 'react'
 import PropTypes from 'prop-types'
+import fp from 'lodash/fp'
+import {mapProps} from 'recompose'
 import Template from '.'
 
-const Preview = ({entry, widgetFor}) => {
-	const props = {
-		...entry.getIn(['data']).toJS(),
-		body: undefined,
+const enhance = mapProps(({entry, widgetFor}) => {
+	// eslint-disable-next-line no-unused-vars
+	const {body, ...data} = entry.getIn(['data']).toJS()
+	return {
+		...data,
+		author: fp.get('author.title')(data),
 		content: widgetFor('body'),
+		siteTitle: 'Campus CMS',
 	}
+})
 
-	return <Template {...props} />
-}
+const Preview = enhance(Template)
 
 Preview.propTypes = {
 	entry: PropTypes.shape({getIn: PropTypes.func}),
