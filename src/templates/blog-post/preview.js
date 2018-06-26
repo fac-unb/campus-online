dafcc249fd0ad143eab57d8fc91b5950b07f56b8
Leonardo Dino <leonardo@kunstdesign.com.br>
2018-06-26 07:42:05 -0300

    templates/blog-post: fix preview

--- src/templates/blog-post/preview.js
+++ src/templates/blog-post/preview.js
@@ -2,21 +2,22 @@ import PropTypes from 'prop-types'
 import {mapProps} from 'recompose'
 import BlogPost from '.'
 
-const expand = data =>
+const expand = (data, prop = 'title') =>
 	((data || {}).slug || null) && {
 		url: data.slug,
-		title: data.title,
+		[prop]: data.title,
 	}
 
 const enhance = mapProps(({entry, widgetFor}) => {
 	// eslint-disable-next-line no-unused-vars
-	const {body, ...data} = entry.getIn(['data']).toJS()
+	const {body, cover, ...data} = entry.getIn(['data']).toJS()
 	return {
 		...data,
-		author: expand(data.author),
-		editorial: expand(data.editorial),
 		content: widgetFor('body'),
 		siteTitle: 'CMS',
+		cover: (cover || null) && {image: {sizes: {src: cover}}},
+		author: expand(data.author, 'name'),
+		editorial: expand(data.editorial),
 	}
 })
 
