f3efdee768e6420fb70d619f22fcc336373b6a57
Leonardo Dino <leonardo@kunstdesign.com.br>
2018-08-12 00:19:58 -0300

    compile functions

--- src/functions/identity-validate.js
+++ src/functions/identity-validate.js
@@ -1,5 +1,16 @@
+export async function handler(event) {
+	const validationError = getValidationError(event)
+	if (validationError) return validationError
+
+	const {
+		user: {email},
+	} = JSON.parse(event.body)
+	if (!email.endsWith('@kunstdesign.com.br')) return reject('wrong acl')
+	return {statusCode: 204, body: ''}
+}
+
 const reject = (...reasons) => {
-	console.log(...reasons)
+	console.log(...reasons) // eslint-disable-line no-console
 	return {statusCode: 403, body: ''}
 }
 
@@ -18,15 +29,4 @@ const getValidationError = event => {
 	if (!json.user) return reject('empty json.user')
 	if (!json.user.email) return reject('empty json.user.email')
 	if (typeof json.user.email !== 'string') return reject('email is not string')
-}
-
-exports.handler = async function(event) {
-	const validationError = getValidationError(event)
-	if (validationError) return validationError
-
-	const {
-		user: {email},
-	} = JSON.parse(event.body)
-	if (!email.endsWith('@kunstdesign.com.br')) return reject('wrong acl')
-	return {statusCode: 204, body: ''}
 }
