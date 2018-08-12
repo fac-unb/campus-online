877d6de0c26481c59f873f3ce169c0464f692c28
Leonardo Dino <leonardo@kunstdesign.com.br>
2018-08-11 23:24:23 -0300

    netlify: test identity-validate

--- functions/identity-validate.js
+++ functions/identity-validate.js
@@ -1,4 +1,32 @@
-exports.handler = async event => {
-	console.log('EVENT:', event.body)
+const reject = (...reasons) => {
+	console.log(...reasons)
 	return {statusCode: 403, body: ''}
+}
+
+const getValidationError = event => {
+	if (!event) return reject('event missing')
+	if (!event.body) return reject('event.body missing')
+	let json = {}
+
+	try {
+		json = JSON.parse(event.body)
+	} catch (e) {
+		return reject('error parsing json', typeof event.body)
+	}
+
+	if (!json) return reject('empty json')
+	if (!json.user) return reject('empty json.user')
+	if (!json.user.email) return reject('empty json.user.email')
+	if (typeof json.user.email !== 'string') return reject('email is not string')
+}
+
+exports.handler = async function(event) {
+	const validationError = getValidationError(event)
+	if (validationError) return validationError
+
+	const {
+		user: {email},
+	} = JSON.parse(event.body)
+	if (!email.endsWith('@kunstdesign.com.br')) return reject('wrong acl')
+	return {statusCode: 204, body: ''}
 }
