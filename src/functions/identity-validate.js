fd7c4cbcdb5cf0978df3ca2cbeccf385c5029f94
Leonardo Dino <leonardo@kunstdesign.com.br>
2018-08-12 12:17:46 -0300

    functions: production code

--- src/functions/identity-validate.js
+++ src/functions/identity-validate.js
@@ -1,31 +1,26 @@
 import {createHash} from 'crypto'
 import fetch from 'node-fetch'
-import LRU from 'lru-cache'
 
 const sha256 = string =>
 	createHash('sha256')
 		.update(string)
 		.digest('hex')
 const fetchJSON = async (...args) => await (await fetch(...args)).json()
-const cache = LRU({max: 64, maxAge: 60 * 1000})
 const isAllowed = async email => {
 	if (typeof email !== 'string') throw new Error('email is not a string')
-	// if (cache.has(email)) return cache.get(email)
 	const url = `https://auth-netlify.firebaseio.com/acl/${sha256(email)}.json`
 	const value = await fetchJSON(`${url}?shallow=true`)
 	console.log({email, value, hash: sha256(email)}) // eslint-disable-line
-	const result = email === value
-	return cache.set(email, result), result
+	return email === value
 }
 
 export const handler = async ({body = 'null'} = {}) => {
 	try {
 		const {email} = JSON.parse(body).user
 		if (await isAllowed(email)) return {statusCode: 204, body: ''}
-		// throw new Error(`email '${email}' is not allowed`)
+		throw new Error(`email '${email}' is not allowed`)
 	} catch ({message}) {
 		console.log(message) // eslint-disable-line
 	}
-	return {statusCode: 204, body: ''}
-	// return {statusCode: 403, body: ''}
+	return {statusCode: 403, body: ''}
 }
