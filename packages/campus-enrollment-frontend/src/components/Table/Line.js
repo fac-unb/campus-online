c24743e4b4b8d4ee70ada5813214bc97f5766020
Vitor Dino <vitor@kunstdesign.com.br>
2018-09-02 20:51:43 -0300

    enrollment-ui: Table: add Checkbox

--- packages/campus-enrollment-frontend/src/components/Table/Line.js
+++ packages/campus-enrollment-frontend/src/components/Table/Line.js
@@ -2,39 +2,51 @@ import React from 'react'
 import styled from 'styled-components'
 import {colors} from '../../constants'
 import {above} from '../../utils/responsive'
+import Checkbox from '../Checkbox'
 
 const Wrapper = styled.div`
-	overflow: hidden;
-	padding: 1rem 0rem;
 	line-height: 1.5rem;
 	border-bottom: 1px solid ${colors.base11};
+	display: flex;
+	align-items: center;
+`
+
+const Flex = styled.div`
+	padding: 0.5rem;
 	${above.md`
+		padding: 0;
+		flex: 1;
 		display: flex;
 		align-items: center;
 		justify-content: space-between;
-		padding: 1rem 0.5rem;
 	`}
 `
 
 const Name = styled.div`
 	font-weight: 600;
+	${above.md`padding: 1rem 0.5rem;`}
 `
 
 const Mail = styled.div`
 	color: ${colors.base88};
+	${above.md`padding: 1rem 0.5rem;`}
 `
 
 const When = styled.div`
 	font-size: 0.875rem;
 	font-weight: 500;
 	color: ${colors.base44};
+	${above.md`padding: 1rem 0.5rem;`}
 `
 
 const Line = ({name, mail, date}) => (
 	<Wrapper>
-		<Name>{name}</Name>
-		<Mail>{mail}</Mail>
-		<When>{date}</When>
+		<Checkbox style={{flex: 0}}/>
+		<Flex>
+			<Name>{name}</Name>
+			<Mail>{mail}</Mail>
+			<When>{date}</When>
+		</Flex>
 	</Wrapper>
 )
 
