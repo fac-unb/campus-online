67a00bcb6b1e039ec5c4878df462c70fe357a592
Vitor Dino <vitor@kunstdesign.com.br>
2018-09-04 05:13:37 -0300

    campus-enrollment: Navbar: reduce dark pattern on AuthButton

--- packages/campus-enrollment-frontend/src/components/Navbar/AuthButton.js
+++ packages/campus-enrollment-frontend/src/components/Navbar/AuthButton.js
@@ -5,7 +5,8 @@ import AuthState from '../AuthState'
 import Icon from '../Icon'
 
 const Wrapper = styled.button`
-	display: block;
+	display: flex;
+	align-items: stretch;
 	border: none;
 	margin: 0;
 	padding: 0;
@@ -17,18 +18,21 @@ const Wrapper = styled.button`
 	cursor: pointer;
 	padding: 0.5rem;
 	margin-right: -0.5rem;
+	user-select: none;
 `
 
 const Inner = styled.div`
+	position: relative;
 	background: ${colors.base11};
+	color: ${colors.base66};
 	width: 2.5rem;
 	height: 2.5rem;
-	border-radius: 0.25rem;
-	color: ${colors.base66};
 	display: flex;
 	align-items: center;
 	justify-content: center;
-	:hover{
+	border-top-right-radius: 0.25rem;
+	border-bottom-right-radius: 0.25rem;
+	${Wrapper}:hover &{
 		background: ${colors.base88};
 		color: ${colors.base03};
 	}
@@ -37,10 +41,35 @@ const Inner = styled.div`
 	}
 `
 
-const Button = ({icon, onClick, props}) => (
+const Text = styled.div`
+	display: flex;
+	align-items: center;
+	font-size: 0.75rem;
+	text-transform: uppercase;
+	font-weight: 700;
+	line-height: 1.25rem;
+	top: 0;
+	bottom: 0;
+	right: 100%;
+	background: ${colors.base11};
+	color: ${colors.base66};
+	white-space: nowrap;
+	padding: 0.625rem 0.125rem 0.625rem 1rem;
+	border-top-left-radius: 0.25rem;
+	border-bottom-left-radius: 0.25rem;
+	${Wrapper}:hover &{
+		background: ${colors.base88};
+		color: ${colors.base03};
+	}
+`
+
+
+
+const Button = ({icon, text, onClick, props}) => (
 	<Wrapper {...props} onClick={onClick}>
+		<Text>{text}</Text>
 		<Inner>
-			<Icon size={20} icon={icon}/>
+			<Icon size={18} icon={icon} strokeWidth={2.125}/>
 		</Inner>
 	</Wrapper>
 )
@@ -49,6 +78,7 @@ const External = ({render, children = render}) => (
 	<AuthState
 		render={({isLoggedIn, action}) => children({
 			icon: isLoggedIn ? 'log-out' : 'log-in',
+			text: isLoggedIn ? 'Log out' : 'Log in',
 			onClick: action, disabled: !action,
 		})}
 	/>
