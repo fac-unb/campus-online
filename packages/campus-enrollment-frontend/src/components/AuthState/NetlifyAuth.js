752068fa7a3f61242069a7a2dba74abd5f1d6b90
Leonardo Dino <leonardo@kunstdesign.com.br>
2018-09-03 23:39:02 -0300

    campus-enrollment: pass netlify client id as environment variable

--- packages/campus-enrollment-frontend/src/components/AuthState/NetlifyAuth.js
+++ packages/campus-enrollment-frontend/src/components/AuthState/NetlifyAuth.js
@@ -3,7 +3,7 @@ import {login, logout} from '../../reducers/auth'
 
 const config = {
 	url: 'https://app.netlify.com/authorize',
-	client: '3905bdb7e9bb641c2e5a5a8ae4b845e9a6181dcb2e8ee6337e226e11593a1325',
+	client: process.env.REACT_APP_NETLIFY_CLIENT_ID,
 	redirect: window.location.href,
 	width: 512, // Width, in pixels, of popup window. Optional, default: 400
 	height: 400 // Height, in pixels, of popup window. Optional, default: 400
