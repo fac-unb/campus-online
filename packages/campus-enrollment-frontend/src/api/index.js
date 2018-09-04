2bb5ece98446272abfc0333e9b877c713ab9a5e8
Leonardo Dino <leonardo@kunstdesign.com.br>
2018-09-04 05:20:37 -0300

    enrolment-frontend: add getEnrolledStudents and getNetlifySiteData

--- packages/campus-enrollment-frontend/src/api/index.js
+++ packages/campus-enrollment-frontend/src/api/index.js
@@ -1,1 +1,3 @@
 export {default as getFirebaseToken} from './getFirebaseToken'
+export {default as getEnrolledStudents} from './getEnrolledStudents'
+export {default as getNetlifySiteData} from './getNetlifySiteData'
