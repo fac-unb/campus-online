2fac6ed9942303416217239d4b7768ebfa6e98ff
Leonardo Dino <leonardo@kunstdesign.com.br>
2018-09-04 09:23:38 -0300

    enrolment-frontend: reducers/students: add selected states

--- packages/campus-enrollment-frontend/src/reducers/students.js
+++ packages/campus-enrollment-frontend/src/reducers/students.js
@@ -1,4 +1,5 @@
 import {createAction, handleActions} from 'redux-actions'
+import {compose} from 'recompose'
 import * as api from '../api'
 import * as netlify from './netlify'
 
@@ -7,31 +8,52 @@ const getSortedByDateArray = byId => Object.keys(byId).sort(
 	(a, b) => new Date(byId[a].date) - new Date(byId[b].date)
 )
 
-const idleState = {byId: {}, allIds: [], isFetching: false}
+const uniq = array => [...new Set(castArray(array))]
+const intersect = (array1, array2) => array1.filter(x => array2.includes(x))
+const uniqSect = compose(uniq, intersect)
+
+const idleState = {byId: {}, allIds: [], selectedIds: [], isFetching: false}
 const initialState = {...idleState, isWaiting: true}
 
 // [ACTIONS]
 export const fetching = createAction('enrollment/students/fetching')
 export const receive  = createAction('enrollment/students/receive')
 export const error    = createAction('enrollment/students/error')
+export const select   = createAction('enrollment/students/select')
+export const unselect = createAction('enrollment/students/unselect')
+export const toggle   = createAction('enrollment/students/toggle')
 
 // [REDUCER]
-export default handleActions({
-	[error]:         (state, {payload}) => ({...idleState, error: payload}),
+const reducer = handleActions({
 	[fetching]:      () => ({...idleState, isFetching: true}),
-	[receive]:       (state, {payload = []}) => {
-		const students = castArray(payload).reduce(
+	[netlify.error]: () => idleState,
+	[netlify.clear]: () => initialState,
+	[error]:         (state, {payload}) => ({...idleState, error: payload}),
+	[receive]:       (state, {payload}) => {
+		const students = castArray(payload || []).reduce(
 			(obj, item) => (item || {}).id ? ({...obj, [item.id]: item}) : obj, {},
 		)
 		const byId = {...state.byId, ...students}
-		return {...state, byId, allIds: getSortedByDateArray(byId)}
+		const allIds = getSortedByDateArray(byId)
+		const selectedIds = uniqSect(state.selectedIds, allIds)
+		return {...state, byId, allIds, selectedIds}
 	},
-	[netlify.error]: () => idleState,
-	[netlify.clear]: () => initialState,
+	[select]: ({selectedIds, ...state}, {payload: id}) => ({...state,
+		selectedIds: uniqSect([...selectedIds, id], state.allIds),
+	}),
+	[unselect]: ({selectedIds, ...state}, {payload: id}) => ({...state,
+		selectedIds: uniqSect(selectedIds.filter(a => a !== id), state.allIds),
+	}),
+	[toggle]: (state, {payload: id}) => (
+		reducer(state, state.selectedIds.includes(id) ? unselect(id) : select(id))
+	),
 }, initialState)
+
+export default reducer
 
 // [MIDDLEWARE]
 export const middleware = store => {
+	// [TODO]: handle firebase user tasks
 	return next => async action => {
 		next(action)
 		if(action.type !== String(netlify.replace)) return
