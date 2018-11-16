27ac00bc3fadc11f2f25a4b5ccae269583d8d33e
Vitor Dino <vitor@kunstdesign.com.br>
2018-11-16 02:32:02 -0200

    components: add ArticleHero component

---
+++ packages/campus-online-frontend/src/components/ArticleHero/Image.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+import GatsbyImage from 'gatsby-image'
+
+const ImageWrapper = styled(GatsbyImage)`
+	height: 100%;
+	min-width: 100%;
+	background: #e5e5e5;
+	& img[src^='data:'] {
+		filter: blur(1rem) grayscale(25%);
+		transform: scale(1.025);
+		background: #f0f;
+	}
+`
+
+const Image = ({image, position = 'relative'}) => (
+	<ImageWrapper {...image} style={{position}} position={position} />
+)
+
+export default Image
